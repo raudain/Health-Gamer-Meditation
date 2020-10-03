e59b4194d977972a691e39f1285ce0856f29cb99
Roody <roody.audain@gmail.com>
2020-10-03 13:34:05 -0400

    Added other than youtube links
    I am about to remove "@media screen and (max-width: 1500px" from style.css but I am committing for future reference. I am hard coding the video class height and width.

--- WebContent/Main.js
+++ WebContent/Main.js
@@ -1,11 +1,40 @@
 // JavaScript source code
-document.querySelectorAll(".video-image").forEach(linkElement => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            linkElement.innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send();
-});
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		myfunction(this);
+	}
+};
+xhttp.open("GET", "videos.xml", true);
+xhttp.send();
+
+function myfunction(xml) {
+	const xmlDoc = xml.responseXML;
+	const videoContainer = document.getElementById("videos");
+	const xmlVideos = xmlDoc.getElementsByTagName("video");
+	for (let i = 0; i < xmlVideos.length; i++) {
+		const videoElement = document.createElement("div");
+		videoElement.classList.add("video");
+		videoContainer.appendChild(videoElement);
+
+		const xmlVideo = xmlVideos[i];
+
+		const imageLink = document.createElement("a");
+		const xmlLink = xmlVideo.getElementsByTagName("link")[0].childNodes[0].nodeValue;
+		imageLink.setAttribute("href", xmlLink);
+		videoElement.appendChild(imageLink);
+		
+		const imageElement = document.createElement("img");
+		const imageSource = xmlVideo.getElementsByTagName("source")[0].childNodes[0].nodeValue;
+		imageElement.setAttribute("src", imageSource);
+		imageLink.appendChild(imageElement);
+
+		const captionLink = document.createElement("a");
+		const xmlCaption = xmlVideo.getElementsByTagName("caption")[0].childNodes[0].nodeValue;
+		captionLink.classList.add("video-title");
+		captionLink.setAttribute("title", xmlCaption);
+		captionLink.setAttribute("href", xmlLink);
+		captionLink.innerHTML = xmlCaption;
+		videoElement.appendChild(captionLink);
+	}
+}
